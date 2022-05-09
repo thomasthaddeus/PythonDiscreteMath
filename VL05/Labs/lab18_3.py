7c50e257a731b4caaaf81305a512ce076870f4ba
deusstrong <thomasthaddeus@cityuniversity.edu>
2022-05-09 05:33:49 -0600

    string_formatting

--- VL05/Labs/lab18_3.py
+++ VL05/Labs/lab18_3.py
@@ -1,32 +1,33 @@
 '''18.3 LAB: Count characters
 
-Write a program whose input is a string which contains a character and a phrase,
-    and whose output indicates the number of times the character appears in the phrase.
-    The output should include the input character and use the plural form,
-    n's, if the number of times the characters appears is not exactly 1.
-########################
 Ex: If the input is:
-n Monday
-
+    n Monday
 the output is:
-1 n
+    1 n
 #######################
 Ex: If the input is:
-z Today is Monday
-
+    z Today is Monday
 the output is:
-0 z's
+    0 z's
 #######################
 Ex: If the input is:
-n It's a sunny day
-
+    n It's a sunny day
 the output is:
-2 n's
+    2 n's
 #######################
 Case matters. n is different than N.
 Ex: If the input is:
-n Nobody
-
+    n Nobody
 the output is:
 0 n's
 '''
+import re
+
+
+def count_char(char, phrase):
+    """Write a program whose input is a string which contains a character and a phrase,
+    and whose output indicates the number of times the character appears in the phrase.
+    The output should include the input character and use the plural form,
+    n's, if the number of times the characters appears is not exactly 1."""
+    for char in len(phrase):
+        if char
