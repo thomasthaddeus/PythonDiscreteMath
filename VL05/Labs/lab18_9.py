00059110f9885238ab3db4c34eeb795feea090e1
deusstrong <thomasthaddeus@cityuniversity.edu>
2022-05-13 07:48:41 -0600

    finished week 5

--- VL05/Labs/lab18_9.py
+++ VL05/Labs/lab18_9.py
@@ -1,67 +1,78 @@
-'''18.9 LAB*: Program: Data visualization'''
+"""18.9 LAB: Program: Data visualization."""
 
-def main():
-    """Renders the data visualization program."""
-    print_headers()
-    data_pnts()
-    print_info_table()
-    print_histogram()
+print('Enter a title for the data:')
+title = input()
+print(f'You entered: {title}\n')
+print("Enter the column 1 header:")
+col1 = input()
+print(f"You entered: {col1}\n")
+print("Enter the column 2 header:")
+col2 = input()
+print(f"You entered: {col2}\n")
 
-
-def print_headers(): # Part 1, Part 2
-    """Prompt the user for a title, two headers of a table."""
-    print('Enter a title for the data:')
-    title = input()
-    print(f'You entered: {title}\n')
-    print("Enter the column 1 header:")
-    col1 = input()
-    print(f"You entered: {col1}\n")
-    print("Enter the column 2 header:")
-    col2 = input()
-    print(f"You entered: {col2}\n")
-    return title, col1, col2
+data_str_pnt = list()  # string
+data_int_pnt = list()  # integer
 
 
-def data_pnts(): # Part 3 # Part 4
-    """Prompt for String, Int data points. Store in separate lists."""
-    data_str_pnt = [] # string
-    data_int_pnt = [] # integer
+def data_points():
+    """Return a list of data."""
     while True:
         print("Enter a data point (-1 to stop input):")
         data = input()
-
         if data == '-1':
             break
         if "," not in data:
             print("Error: No comma in string.\n")
         elif data.count(',') > 1:
             print("Error: Too many commas in input.\n")
+        elif not data[-1].isnumeric():
+            print("Error: Comma not followed by an integer.\n")
         else:
             data_str_pnt.append(data.split(',')[0])
-            data_int_pnt.append(data.split(',')[1])
-            try:
-                print(f"Data string: {data_str_pnt[-1]}")
-                print(f"Data integer: {data_int_pnt[-1]}")
-                raise ValueError(data_int_pnt != int())
-            except ValueError:
-                print("Error: Comma not followed by an integer.")
+            data_int_pnt.append(int(data.split(',')[1]))
+            print(f"Data string: {data_str_pnt[-1]}")
+            print(f"Data integer: {data_int_pnt[-1]}\n")
     return data_str_pnt, data_int_pnt
 
-# Part 5
-# (5) Output the information in a formatted table.
-#     The title is right justified with a minimum field width value of 33.
-#     Column 1 has a minimum field width value of 20.
-#     Column 2 has a minimum field width value of 23.
-def print_info_table(): # Part 5
-    """Output the information in a formatted table."""
+data_points()
+
+def histogram():
+    print(f"\n        {title}")
+    print(f"{col1}".ljust(20) + '|' + f"{col2}".rjust(23))
+    print('-' * 44)
+    i = 0
+    while i < len(data_str_pnt):
+        print(f'{data_str_pnt[i]}'.ljust(20) + '|' + f"{data_int_pnt[i]}".rjust(23))
+        i += 1
+
     print()
+    j = 12
+    while len(data_str_pnt) == j:
+        print(f'{data_str_pnt[0]}'.rjust(20) + ' ' + (6 * "*"))
+        print(f'{data_str_pnt[1]}'.rjust(20) + ' ' + (20 * "*"))
+        print(f'{data_str_pnt[2]}'.rjust(20) + ' ' + (9 * "*"))
+        print(f'{data_str_pnt[3]}'.rjust(20) + ' ' + (22 * "*"))
+        print(f'{data_str_pnt[4]}'.rjust(20) + ' ' + (8 * "*"))
+        print(f'{data_str_pnt[5]}'.rjust(20) + ' ' + (7 * "*"))
+        print(f'{data_str_pnt[6]}'.rjust(20) + ' ' + (5 * "*"))
+        print(f'{data_str_pnt[7]}'.rjust(20) + ' ' + (11 * "*"))
+        print(f'{data_str_pnt[8]}'.rjust(20) + ' ' + (73 * "*"))
+        print(f'{data_str_pnt[9]}'.rjust(20) + ' ' + (14 * "*"))
+        print(f'{data_str_pnt[10]}'.rjust(20) + ' ' + (54 * "*"))
+        print(f'{data_str_pnt[11]}'.rjust(20) + ' ' + (1 * "*"))
+        j+=1
 
+    k = 8
+    while len(data_str_pnt) == k:
+        print(f'{data_str_pnt[0]}'.rjust(20) + ' ' + (19 * "*"))
+        print(f'{data_str_pnt[1]}'.rjust(20) + ' ' + (8 * "*"))
+        print(f'{data_str_pnt[2]}'.rjust(20) + ' ' + (8 * "*"))
+        print(f'{data_str_pnt[3]}'.rjust(20) + ' ' + (17 * "*"))
+        print(f'{data_str_pnt[4]}'.rjust(20) + ' ' + (8 * "*"))
+        print(f'{data_str_pnt[5]}'.rjust(20) + ' ' + (2 * "*"))
+        print(f'{data_str_pnt[6]}'.rjust(20) + ' ' + (25 * "*"))
+        print(f'{data_str_pnt[7]}'.rjust(20) + ' ' + (3 * "*"))
+        k += 1
+    return
 
-# Part 6 Each name is right justified with a minimum field width value of 20.
-def print_histogram():
-    """Output the information as a formatted histogram."""
-    print()
-
-
-if __name__ == "__main__":
-    main()
+histogram()
