d27df50b19e3f30caaa6cb09020c252db821badd
deusstrong <thomasthaddeus@cityuniversity.edu>
2022-05-15 05:20:12 -0600

    week 6, 7 and some of 8

---
+++ VL07/HOP/HOP_21_12_1.py
@@ -0,0 +1,11 @@
+colors = ['black', 'green', 'blue', 'red']
+for (position, color) in enumerate(colors):
+    print(position, color)
+
+
+numbers = [-2, -7, 7, 0, 3]
+for (position, number) in enumerate(numbers):
+    if number < 0:
+        print(position, 'x')
+    else:
+        print(position, number)
---
+++ schol.py
@@ -0,0 +1,28 @@
+"""Summary of school cost."""
+class Tuition():
+    """Tuition _summary_"""
+
+
+    def main():
+        """Return the tuition for the year borken down by quarter."""
+        quarter()
+
+
+    def quarter(x, y, z):
+        """Return the cost of tuition"""
+        x = 2534.00
+        y = 2475.00
+        z = 2550.00
+        sub_tot = x + y + z
+        print(f"{'-' * 34} Quarter initial cost: {sub_tot:.2f}")
+        interest = sub_tot * float(0.25)
+        print(f"\tAmazon discount: {interest:.2f}\n{'-' * 34}")
+        first = sub_tot - interest
+        print(f"\tFirst balance: {first:.2f}")
+        total = first - (first * float(0.60))
+        print(f"""\n\tGI Bill savings: {first - total:.2f}\n\tRemaining Balance: {total:.2f}\n""")
+        return total
+
+
+    if __name__ == '__main__':
+        main()
