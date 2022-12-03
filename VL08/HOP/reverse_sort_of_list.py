1049ae34f87b3b0142a7cad29a2692b35e878a3d
Thaddeus Thomas <thomasthaddeus@cityuniversity.edu>
2022-12-02 20:21:30 -0700

    vl08

--- VL08/HOP/reverse_sort_of_list.py
+++
@@ -1,9 +0,0 @@
-"""24.2.1: Reverse sort of list.
-
-Sort short_names in reverse alphabetic order."""
-
-user_input = input()  # 'Jan Sam Ann Joe Tod'
-short_names = user_input.split()
-short_names.sort()
-short_names.reverse()
-print(short_names)  # ['Tod', 'Sam', 'Joe', 'Jan', 'Ann']
